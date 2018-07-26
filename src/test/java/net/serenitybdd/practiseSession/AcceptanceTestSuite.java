7fd22facc98e2ed4c941cd3181deac92b1aaa554
Donthi <Sridevi.Donthi@unisys.com>
2018-07-26 15:40:09 -0400

    first commit

---
+++ src/test/java/net/serenitybdd/practiseSession/AcceptanceTestSuite.java
@@ -0,0 +1,13 @@
+package net.serenitybdd.practiseSession;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+//import net.serenitybdd.jbehave.SerenityStories;
+//@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(features="src/test/resources/features/restapi/checkUserapi.feature")
+public class AcceptanceTestSuite {
+	
+}
